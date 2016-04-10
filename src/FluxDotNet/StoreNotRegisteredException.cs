ad111af81a1af61f8e01b04757cbfbeb54a46853
Steven Atkinson <steven.atkinson@gmx.com>
2016-04-10 20:06:44 +0100

    Added the IStoreResolver and DefaultStoreResolver so we can register and retrieve stores.

---
+++ src/FluxDotNet/StoreNotRegisteredException.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace FluxDotNet
+{
+    public class StoreNotRegisteredException : Exception
+    {
+        public StoreNotRegisteredException()
+            : base("Store has not been registered")
+        {
+        }
+    }
+}
---
+++ src/FluxDotNet/StoreResolver.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace FluxDotNet
+{
+    public interface IStoreResolver
+    {
+        void Register<T>(T store) where T : Store;
+
+        T GetStore<T>() where T : Store;
+    }
+
+    public class DefaultStoreResolver : IStoreResolver
+    {
+        private readonly IDictionary<Type, Store> _stores = new Dictionary<Type, Store>();
+
+        public void Register<T>(T store) where T : Store
+        {
+            var key = typeof(T);
+
+            if (!_stores.ContainsKey(key))
+                _stores.Add(key, null);
+
+            _stores[key] = store;
+        }
+
+        public T GetStore<T>() where T : Store
+        {
+            var key = typeof(T);
+
+            if (!_stores.ContainsKey(key))
+                throw new StoreNotRegisteredException();
+
+            return (T)_stores[key];
+        }
+    }
+}
