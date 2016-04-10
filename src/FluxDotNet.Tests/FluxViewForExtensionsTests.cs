ec94105dbc41c7b33e1e84ae53ccb615a6861bb4
Steven Atkinson <steven.atkinson@gmx.com>
2016-04-10 20:43:38 +0100

    Added tests for the FluxViewForExtensions.

---
+++ src/FluxDotNet.Tests/FluxViewForExtensionsTests.cs
@@ -0,0 +1,46 @@
+﻿using FluxDotNet.Tests.Mocks;
+using NUnit.Framework;
+
+namespace FluxDotNet.Tests
+{
+    [TestFixture]
+    public class FluxViewForExtensionsTests
+    {
+        private IFluxViewFor<TestStore> _view;
+
+        [SetUp]
+        public void SetUp()
+        {
+            var dispatcher = Flux.Dispatcher;
+            var store = new TestStore(dispatcher);
+
+            Flux.StoreResolver.Register(store);
+
+            _view = new TestView();
+        }
+
+        [TearDown]
+        public void TearDown()
+        {
+            _view = null;
+        }
+
+        [Test]
+        public void DispatchShouldCallOnChangeAndUpdateTestResult()
+        {
+            var payload = new TestAction {Result = "Changed"};
+
+            _view.Dispatch(payload);
+
+            Assert.AreEqual(payload.Result, ((TestView)_view).TestResult);
+        }
+
+        [Test]
+        public void EmitChangeShouldDoNothing()
+        {
+            _view.EmitChange();
+
+            Assert.IsNull(((TestView) _view).TestResult);
+        }
+    }
+}
