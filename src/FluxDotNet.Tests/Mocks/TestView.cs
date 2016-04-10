33d099eca3f18caea013e6d59c4b68816f22b53c
Steven Atkinson <steven.atkinson@gmx.com>
2016-04-10 20:26:00 +0100

    Added TestView for testing the IFluxViewFor interface.

---
+++ src/FluxDotNet.Tests/Mocks/TestView.cs
@@ -0,0 +1,17 @@
+﻿namespace FluxDotNet.Tests.Mocks
+{
+    class TestView : IFluxViewFor<TestStore>
+    {
+        public string TestResult { get; private set; }
+
+        public TestView()
+        {
+            this.OnChange(UpdateTestResult);
+        }
+
+        private void UpdateTestResult(TestStore store)
+        {
+            TestResult = store.TestResult;
+        }
+    }
+}
