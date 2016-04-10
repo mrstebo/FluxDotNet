c992eb1fd0431cc1704a1aa237d707adb0e04bad
Steven Atkinson <steven.atkinson@gmx.com>
2016-04-10 19:51:17 +0100

    Added the Dispatcher.

---
+++ src/FluxDotNet/Dispatcher.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace FluxDotNet
+{
+    public interface IDispatcher
+    {
+        void Register<T>(Action<T> callback);
+        void Dispatch<T>(T payload);
+    }
+
+    public class Dispatcher : IDispatcher
+    {
+        private readonly IDictionary<Type, IList<object>> _callbacks = new Dictionary<Type, IList<object>>();
+
+        public void Register<T>(Action<T> callback)
+        {
+            var key = typeof(T);
+
+            if (!_callbacks.ContainsKey(key))
+                _callbacks.Add(key, new List<object>());
+
+            _callbacks[key].Add(callback);
+        }
+
+        public void Dispatch<T>(T payload)
+        {
+            var key = typeof(T);
+
+            if (!_callbacks.ContainsKey(key))
+                return;
+
+            _callbacks[key]
+                .OfType<Action<T>>()
+                .ToList()
+                .ForEach(callback => callback(payload));
+        }
+    }
+}
