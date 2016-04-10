78c58de896d59818d62c5d05759b236f5456fff6
Steven Atkinson <steven.atkinson@gmx.com>
2016-04-10 20:12:29 +0100

    Added DefaultStoreResolver tests.

---
+++ src/FluxDotNet.Tests/DefaultStoreResolverTests.cs
@@ -0,0 +1,66 @@
+﻿using FluxDotNet.Tests.Mocks;
+using NUnit.Framework;
+
+namespace FluxDotNet.Tests
+{
+    [TestFixture]
+    public class DefaultStoreResolverTests
+    {
+        private IStoreResolver _storeResolver;
+
+        [SetUp]
+        public void SetUp()
+        {
+            _storeResolver = new DefaultStoreResolver();
+        }
+
+        [TearDown]
+        public void TearDown()
+        {
+            _storeResolver = null;
+        }
+
+        [Test]
+        public void RegisterShouldNotThrowException()
+        {
+            var dispatcher = new Dispatcher();
+            var store = new TestStore(dispatcher);
+
+            Assert.DoesNotThrow(() => _storeResolver.Register(store));
+        }
+
+        [Test]
+        public void GetStoreShouldReturnRegisteredStore()
+        {
+            var dispatcher = new Dispatcher();
+            var store = new TestStore(dispatcher);
+
+            _storeResolver.Register(store);
+
+            var result = _storeResolver.GetStore<TestStore>();
+
+            Assert.AreSame(store, result);
+        }
+
+        [Test]
+        public void GetStoreShouldReturnLastRegisteredStore()
+        {
+            var dispatcher = new Dispatcher();
+            var store1 = new TestStore(dispatcher);
+            var store2 = new TestStore(dispatcher);
+
+            _storeResolver.Register(store1);
+            _storeResolver.Register(store2);
+
+            var result = _storeResolver.GetStore<TestStore>();
+
+            Assert.AreSame(store2, result);
+        }
+
+        [Test]
+        public void GetStoreWithUnregisteredStoreShouldThrowStoreNotRegisteredException()
+        {
+            Assert.Throws<StoreNotRegisteredException>(() => _storeResolver.GetStore<TestStore>());
+        }
+    }
+}
