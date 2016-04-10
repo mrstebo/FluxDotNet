4166941b4a6bdc92a6e9654802b568f7dcbfea9b
Steven Atkinson <steven.atkinson@gmx.com>
2016-04-10 20:15:14 +0100

    Added Flux class for storing what IDispatcher and IStoreResolver we are using.

---
+++ src/FluxDotNet/Flux.cs
@@ -0,0 +1,14 @@
+﻿namespace FluxDotNet
+{
+    public static class Flux
+    {
+        public static IDispatcher Dispatcher;
+        public static IStoreResolver StoreResolver;
+
+        static Flux()
+        {
+            Dispatcher = new Dispatcher();
+            StoreResolver = new DefaultStoreResolver();
+        }
+    }
+}
