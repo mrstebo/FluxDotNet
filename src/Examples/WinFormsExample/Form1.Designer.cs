88da75804ae88936c0afac397aa12d8668a7ae8d
Steven Atkinson <steven.atkinson@gmx.com>
2016-04-11 08:08:35 +0100

    Added the winforms example project.

---
+++ src/Examples/WinFormsExample/Form1.Designer.cs
@@ -0,0 +1,108 @@
+﻿namespace WinFormsExample
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtNewTodo = new System.Windows.Forms.TextBox();
+            this.btnAdd = new System.Windows.Forms.Button();
+            this.lstTodos = new System.Windows.Forms.ListView();
+            this.colID = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.colDescription = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.SuspendLayout();
+            // 
+            // txtNewTodo
+            // 
+            this.txtNewTodo.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.txtNewTodo.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.txtNewTodo.Location = new System.Drawing.Point(13, 13);
+            this.txtNewTodo.Name = "txtNewTodo";
+            this.txtNewTodo.Size = new System.Drawing.Size(294, 20);
+            this.txtNewTodo.TabIndex = 0;
+            // 
+            // btnAdd
+            // 
+            this.btnAdd.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnAdd.Location = new System.Drawing.Point(313, 11);
+            this.btnAdd.Name = "btnAdd";
+            this.btnAdd.Size = new System.Drawing.Size(75, 23);
+            this.btnAdd.TabIndex = 1;
+            this.btnAdd.Text = "&Add";
+            this.btnAdd.UseVisualStyleBackColor = true;
+            // 
+            // lstTodos
+            // 
+            this.lstTodos.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.lstTodos.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.lstTodos.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
+            this.colID,
+            this.colDescription});
+            this.lstTodos.GridLines = true;
+            this.lstTodos.Location = new System.Drawing.Point(13, 51);
+            this.lstTodos.Name = "lstTodos";
+            this.lstTodos.Size = new System.Drawing.Size(375, 326);
+            this.lstTodos.TabIndex = 2;
+            this.lstTodos.UseCompatibleStateImageBehavior = false;
+            this.lstTodos.View = System.Windows.Forms.View.Details;
+            // 
+            // colID
+            // 
+            this.colID.Text = "ID";
+            // 
+            // colDescription
+            // 
+            this.colDescription.Text = "Description";
+            this.colDescription.Width = 302;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(400, 389);
+            this.Controls.Add(this.lstTodos);
+            this.Controls.Add(this.btnAdd);
+            this.Controls.Add(this.txtNewTodo);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtNewTodo;
+        private System.Windows.Forms.Button btnAdd;
+        private System.Windows.Forms.ListView lstTodos;
+        private System.Windows.Forms.ColumnHeader colID;
+        private System.Windows.Forms.ColumnHeader colDescription;
+    }
+}
+
