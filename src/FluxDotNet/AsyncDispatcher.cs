805a654fc16ec72471a41f1982668eb597c07e74
Steven Atkinson <steven.atkinson@gmx.com>
2016-04-13 19:49:33 +0100

    Added the AsyncDispatcher.

---
+++ src/FluxDotNet/AsyncDispatcher.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace FluxDotNet
+{
+    public interface IAsyncDispatcher
+    {
+        SynchronizationContext SynchronizationContext { get; set; }
+    }
+
+    public class AsyncDispatcher : Dispatcher, IAsyncDispatcher
+    {
+        public SynchronizationContext SynchronizationContext { get; set; }
+
+        public AsyncDispatcher()
+        {
+            SynchronizationContext = AsyncOperationManager.SynchronizationContext;
+        }
+
+        public new void Dispatch<T>(T payload)
+        {
+            var callbacks = GetCallbacks<T>()
+                .ToList();
+
+            if (payload is ChangePayload)
+                callbacks.ForEach(callback => SynchronizationContext.Post(x => callback((T) x), payload));
+            else
+                callbacks.ForEach(callback => Task.Factory.StartNew(() => callback(payload)));
+        }
+    }
+}
